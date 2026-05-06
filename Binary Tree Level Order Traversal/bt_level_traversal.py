a53f3e0b09cc37840367e6b3c5b3709a2f7b788c
Sviatoslav Volianiuk <102855447+Sviat-Vol@users.noreply.github.com>
2026-05-06 10:41:39 +0300

    Implement bt_level_traversal.py
    Вирішено проблему обходу дерева методом рівнів.

---
+++ Binary Tree Level Order Traversal/bt_level_traversal.py
@@ -0,0 +1,30 @@
+""" Binary Tree Level Order Traversal """
+
+class TreeNode:
+    """
+    Tree Node class
+    """
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def level_order(root):
+    """
+    Binary Tree Level Order Traversal
+    """
+    if not root:
+        return []
+    result = []
+    queue = [root]
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+            node = queue.pop(0)
+            level.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        result.append(level)
+    return result
