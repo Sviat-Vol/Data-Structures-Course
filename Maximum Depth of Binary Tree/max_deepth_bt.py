240f1405119755bb4e6569c36949c53d1360479d
Sviatoslav Volianiuk <102855447+Sviat-Vol@users.noreply.github.com>
2026-05-06 10:42:52 +0300

    Implement max_deepth_bt.py
    Вирішено проблему знаходження глибини бінарного дерева.

---
+++ Maximum Depth of Binary Tree/max_deepth_bt.py
@@ -0,0 +1,20 @@
+""" Maximum Depth of Binary Tree """
+
+class TreeNode:
+    """
+    Tree Node class
+    """
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def max_depth(root):
+    """
+    Find maximum depth of binary tree
+    """
+    if not root:
+        return 0
+    left_depth = max_depth(root.left)
+    right_depth = max_depth(root.right)
+    return max(left_depth, right_depth) + 1
