b7f69a27059cfa76ca92d33d9056441cfbe1453e
Sviatoslav Volianiuk <102855447+Sviat-Vol@users.noreply.github.com>
2026-05-06 10:37:50 +0300

    Implement valid_parentheses.py
    Вирішено проблему перевірки вирізу на правильність розставки дужок

---
+++ Valid Parentheses/valid_parentheses.py
@@ -0,0 +1,16 @@
+""" Valid Parentheses """
+
+def is_valid(s):
+    """
+    Check Parentheses
+    """
+    stack = []
+    mapping = {')': '(' , '}': '{' , ']': '['}
+    for char in s:
+        if char in mapping:
+            top_element = stack.pop() if stack else '#'
+            if mapping[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    return not stack
